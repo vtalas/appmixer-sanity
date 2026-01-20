1e2967fab0c5cf3eef9c91e6617889a6ca629ffd
Vladimir Talas <vladimir.talas@appmixer.ai>
2026-01-20 11:06:17 +0100

    update

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import Google from '@auth/core/providers/google';
+import Google from '@auth/sveltekit/providers/google';
 import { env } from '$env/dynamic/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth(async (event) => {
